f2d40de6321af7dfe9e1b73b6630b91f75eab62e
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2018-08-22 10:23:47 -0300

    Added the project to the repository

---
+++ src/login/Controller.java
@@ -0,0 +1,32 @@
+package login;
+
+
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class Controller {
+    @FXML
+    private TextField loginUsuario;
+    @FXML
+    private TextField loginSenha;
+    @FXML
+    private Button loginEnviar;
+
+    public void cadastrarAction() {
+        System.out.println("alô");
+    }
+    public void enviarAction() throws Exception {
+        System.out.println(loginUsuario.getText());
+        Parent root = FXMLLoader.load(getClass().getResource("../home/home.fxml"));
+        loginEnviar.getScene().getWindow().hide();
+        Stage homeStage = new Stage();
+        homeStage.setTitle("Home - Baitas Tarefas");
+        homeStage.setScene(new Scene(root, 800, 600));
+        homeStage.show();
+    }
+}
